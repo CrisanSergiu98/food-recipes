683254db143cac98f9c85ee1e2de8e9775e07deb
unknown <cdsergiu@outlook.com>
2025-01-10 03:29:12 +0200

    Updated docuentation

--- FoodRecipes.Application/Abstractions/Repositories/IIngredientRepository.cs
+++ FoodRecipes.Application/Abstractions/Repositories/IIngredientRepository.cs
@@ -3,25 +3,19 @@ using FoodRecipes.Domain.Ingredients;
 
 namespace FoodRecipes.Application.Abstractions.Repositories;
 
-// Repository interface for managing Ingredient entities
 public interface IIngredientRepository : IRepository<Ingredient>
 {
-    // Get an ingredient by its ID
     Task<Ingredient?> GetById(Guid id, CancellationToken cancellationToken);
 
-    // Get all ingredients
     Task<List<Ingredient>> GetAll(CancellationToken cancellationToken);
 
     Task<List<Ingredient>> SearchByName(string name, CancellationToken cancellationToken);
 
     Task<bool> NameExists(string name, CancellationToken cancellationToken);
 
-    // Delete an ingredient by its ID
     void Delete(Ingredient ingredient);
 
-    // Insert a new ingredient
     void Insert(Ingredient ingredient);
 
-    // Update an existing ingredient
     void Update(Ingredient ingredient);
 }
--- FoodRecipes.Application/Abstractions/Repositories/IRecipeRepository.cs
+++ FoodRecipes.Application/Abstractions/Repositories/IRecipeRepository.cs
@@ -3,25 +3,19 @@ using FoodRecipes.Domain.Recipes;
 
 namespace FoodRecipes.Application.Abstractions.Repositories;
 
-// Repository interface for managing Recipe entities
 public interface IRecipeRepository : IRepository<Recipe>
 {
-    // Get a recipe by its ID
     Task<Recipe?> GetById(Guid id, CancellationToken cancellationToken = default);
 
-    // Get all recipes
     Task<List<Recipe>> GetAll(CancellationToken cancellationToken = default);
 
     Task<List<Recipe>> SearchByTitle(string title, CancellationToken cancellationToken);
 
     Task<bool> TitleExists(string title, CancellationToken cancellationToken);
 
-    // Update an existing recipe
     void Update(Recipe recipe);
 
-    // Delete a recipe by its ID
     void Delete(Recipe recipe);
 
-    // Insert a new recipe
     void Insert(Recipe recipe);
 }
